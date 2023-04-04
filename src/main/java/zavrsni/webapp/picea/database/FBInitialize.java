201bf00f89bbc28d980d75c622944b0a0bb2d0f5
Jura <jura.starcevic@mail.com>
2023-04-04 10:58:39 +0200

    added create Producer

--- src/main/java/zavrsni/webapp/picea/database/FBInitialize.java
+++ src/main/java/zavrsni/webapp/picea/database/FBInitialize.java
@@ -7,12 +7,14 @@ import com.google.cloud.firestore.*;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -39,40 +41,42 @@ public class FBInitialize {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         new FBInitialize().initialize();
 
-        Firestore firestore = FirestoreClient.getFirestore();
+        addNewProducer();
 
-        CollectionReference collectionRef = firestore.collection("Producer");
-
-        Query query = collectionRef;
-
-        ApiFuture<QuerySnapshot> querySnapshot = query.get();
-
-
-        for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
-            System.out.println(document.getId() + " => " + document.getData().get("producerName"));
-        }
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        DatabaseReference usersRef = database.getReference("Producer");
-
-        Producer producer = new Producer("Producer 1");
-
-
+        printProducers();
 
     }
 
-    private static class Producer{
-        private String producerName;
-        private String producerId;
+    private static void addNewProducer() {
+        Firestore db = FirestoreClient.getFirestore();
 
-        public Producer(String producerName, String producerId){
-            this.producerName = producerName;
-            this.producerId = producerId;
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        ref = ref.child("Producer").push();
+
+
+        ApiFuture<WriteResult> collectionsApiFuture = db.collection("Producer").document(ref.getKey()).set(new Producer("huehuehue"));
+        while (!collectionsApiFuture.isDone()) {
+            System.out.println("Waiting for producer to be added");
         }
 
-        public Producer(String producerName){
-            this.producerName = producerName;
+    }
+
+    private static void printProducers() {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> query = db.collection("Producer").get();
+        QuerySnapshot querySnapshot = null;
+        try {
+            querySnapshot = query.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+            System.out.println(document.getData());
         }
     }
+
+
 }
---
+++ src/main/java/zavrsni/webapp/picea/database/Producer.java
@@ -0,0 +1,13 @@
+package zavrsni.webapp.picea.database;
+
+public class Producer {
+    public String producerName;
+
+    public Producer(String producerName) {
+        this.producerName = producerName;
+    }
+
+    public String getProducerName() {
+        return producerName;
+    }
+}
