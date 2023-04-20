9c65e264d59ea1bec3686035d4615c86b5ff8bc5
Sarmaguy <jura.starcevic@fer.hr>
2023-04-20 18:15:10 +0200

    DBModel Sowing

--- src/main/java/zavrsni/webapp/picea/database/models/Sowing.java
+++ src/main/java/zavrsni/webapp/picea/database/models/Sowing.java
@@ -1,82 +1,88 @@
 package zavrsni.webapp.picea.database.models;
 
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 
 import java.util.List;
 
 public class Sowing extends DatabaseModel {
-    private int year;
+
     private String producerId;
     private String sortId;
-    private String sowingId;
+    private int year;
 
-    public Sowing(int year, String producerId, String sortId, String sowingId) {
+    public Sowing(String id, String producerId, String sortId, int year) {
+        super(id);
+        this.producerId = producerId;
+        this.sortId = sortId;
         this.year = year;
-        producerId = producerId;
-        sortId = sortId;
-        this.sowingId = sowingId;
+    }
+
+    public Sowing(String producerId, String sortId, int year) {
+        this("", producerId, sortId, year);
     }
 
     public Sowing() {
-        this(0, "", "","");
-    }
-
-    public int getYear() {
-        return year;
+        this("", "", "", 0);
     }
 
     public String getProducerId() {
         return producerId;
     }
 
-    public String getSortId() {
-        return sortId;
-    }
-
-    @Override
-    public String toString() {
-        return "Sowing{" +
-                "year=" + year +
-                ", ProducerId='" + producerId + '\'' +
-                ", SortId='" + sortId + '\'' +
-                ", SowingId='" + sowingId + '\'' +
-                '}';
-    }
-
-    public void setYear(int i) {
-        this.year = i;
-    }
-
     public void setProducerId(String producerId) {
         this.producerId = producerId;
+    }
+
+    public String getSortId() {
+        return sortId;
     }
 
     public void setSortId(String sortId) {
         this.sortId = sortId;
     }
 
-    public void setSowingId(String sowingId) {
-        this.sowingId = sowingId;
+    public int getYear() {
+        return year;
     }
 
-    public String getSowingId() {
-        return sowingId;
+    public void setYear(int year) {
+        this.year = year;
     }
 
-    public static List<Sowing> getAll(Firestore db) {
-        return DatabaseModel.getAll(db, Sowing.class);
+    @Override
+    public String toString() {
+        return "Sowing{" +
+                "producerId='" + producerId + '\'' +
+                ", sortId='" + sortId + '\'' +
+                ", year=" + year +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public static ApiFuture<WriteResult> create(Firestore db, Sowing sowing) {
+        return DatabaseModel.create(db, sowing);
+    }
+
+    public static ApiFuture<WriteResult> delete(Firestore db, String id) {
+        return DatabaseModel.delete(db, id, Sowing.class);
     }
 
     public static Sowing get(Firestore db, String id) {
         return DatabaseModel.get(db, Sowing.class, id);
     }
 
-    public static void update(Firestore db, Sowing sowing) {
-        DatabaseModel.update(db, sowing);
+    public static List<Sowing> getAll(Firestore db) {
+        return DatabaseModel.getAll(db, Sowing.class);
     }
 
-    @Override
-    public String getId() {
-        return sowingId;
+    public static ApiFuture<WriteResult> update(Firestore db, Sowing sowing) {
+        return DatabaseModel.update(db, sowing);
     }
 }
